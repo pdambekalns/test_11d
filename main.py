cfaddfa38f95c4ac598fd338e52025f2d66d83f0
pdambekalns <97154866+pdambekalns@users.noreply.github.com>
2022-01-06 06:48:07 +0000

    izpildīts uzdevums

---
+++ main.py
@@ -0,0 +1,11 @@
+mala=float(input("Ievadi malas garumu! "))
+if mala>=5:
+  laukums=mala*mala
+  mala1=mala+5
+  laukums1=mala1*mala1
+  starpiba=(laukums1/laukums)*100
+  print(laukums)
+  print(laukums1)
+  print(starpiba)
+else:
+  print("Nē")
